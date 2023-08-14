8e890f5bcc13a52ab145c8cbae97f357a4076cd1
Lurie <lurainekimmerle@gmail.com>
2023-08-14 11:07:58 +0000

    fill in dummy content

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,16 @@ function About (){
     return (
         <div className="about">
             <h3>About</h3>
-            <p> this is some stuff about Lurie</p>
+            <p> this is some stuff about Lurie
+                This so somemore stuff. And yet
+                I am typimg more things. And one last sentace.
+            </p>
             <h3>Interests</h3>
-            <p>This is some stuff about her interests</p>
+            <p>This is some stuff about her interests.  
+                This is more info on her interstes.  This is 
+                even more stuff about her interests. And yet a few
+                more things to know about Lurie
+            </p>
         </div>
     )
 
