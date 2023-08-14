1857707ba02b2139784d3a9f1e8290f828b9784d
Lurie <lurainekimmerle@gmail.com>
2023-08-14 11:49:46 +0000

    add links to footer

--- src/components/About.js
+++ src/components/About.js
@@ -4,13 +4,13 @@ function About (){
             <h3>About</h3>
             <p> this is some stuff about Lurie
                 This so somemore stuff. And yet
-                I am typimg more things. And one last sentace.
+                I am typimg more things. 
             </p>
             <h3>Interests</h3>
             <p>This is some stuff about her interests.  
                 This is more info on her interstes.  This is 
-                even more stuff about her interests. And yet a few
-                more things to know about Lurie
+                even more stuff about her interests.
+                
             </p>
         </div>
     )
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,18 @@ function Footer(){
 
     return (
         <div className="footer">
-            <img src={GitHubWhite}alt="github"/>
-            <img src={LinkedIn} alt="LinkedIn"/>
-            <img src={Email}alt="email"/>
+            <img src={GitHubWhite}
+                alt="github"
+                href="https://github.com/LurieK/"
+                />
+            <img src={LinkedIn} 
+                alt="LinkedIn"
+                href="https://www.linkedin.com/in/lurie-kimmerle/"
+                />
+            <img src={Email}
+                alt="email"
+                href="mailto:LuraineKimmerle@gmail.com"
+                />
         </div>
     )
 }
