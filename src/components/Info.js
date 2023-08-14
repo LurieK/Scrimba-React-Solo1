b83048c498589d1973d67ab726718adec0f8e099
Lurie <lurainekimmerle@gmail.com>
2023-08-14 09:34:55 +0000

    center info

--- src/components/Info.js
+++ src/components/Info.js
@@ -3,7 +3,7 @@ import Buttons from "./Buttons.js";
 function Info (){
 
     return (
-        <div>
+        <div class="info">
             <h1>Lurie Kimmerle</h1>
             <h3> Frontend Developer</h3>
             <h4>React | JavaScript | CSS </h4>
